b31f6e8b8199e2779c466359d96e0727431215d0
evgageev <evgageev98@gmail.com>
2023-04-16 14:41:59 +0500

    feat: vigenere-cipher 6 passing test

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key, type) {
+    if (!message || !key) {
+      throw new NotImplementedError('Incorrect arguments!');
+    }
+    function checkLetter(letter) {
+      let l = letter.charCodeAt();
+      if ((l >= 65 && l <= 90) || (l >= 97 && l <= 122)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    let cypher = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentLetter = message[i].toUpperCase();
+  
+      if (checkLetter(currentLetter)) {
+        let upperLetter =
+          (currentLetter.charCodeAt() -
+            65 +
+            (key[j % key.length].toUpperCase().charCodeAt() - 65)) %
+          26;
+        cypher += String.fromCharCode(upperLetter + 65);
+        j++;
+      } else {
+        cypher += currentLetter;
+      }
+    }
+    if (type === false) {
+      return cypher.split().reverse().join();
+    }
+    return cypher;
+  }
+
+  decrypt(cypher, key, type) {
+    if (!cypher || !key) {
+      throw new NotImplementedError('Incorrect arguments!');
+    }
+    function checkLetter(letter) {
+      let l = letter.charCodeAt();
+      if ((l >= 65 && l <= 90) || (l >= 97 && l <= 122)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    let message = "";
+    for (let i = 0, j = 0; i < cypher.length; i++) {
+    let currentLetter = cypher[i].toUpperCase();
+
+    if (checkLetter(currentLetter)) {
+      let upperLetter =
+        90 -
+        ((25 -
+          (currentLetter.charCodeAt() -
+            key[j % key.length].toUpperCase().charCodeAt())) %
+          26);
+      message += String.fromCharCode(upperLetter);
+      j++;
+    } else {
+      message += currentLetter;
+    }
+  }
+  if (type === false) {
+    return message.split().reverse().join();
+  }
+  return message;
   }
 }
 
